3d6483e0d9a8fc954752524cc01428009bda348a
rafael <rafael@rafael-Lenovo-G40-70>
2018-01-02 14:00:40 -0200

    Verbos http e convenções de nome de rotas

--- vraptor-produtos/src/main/java/br/com/caelum/vraptor/controller/ProdutoController.java
+++ vraptor-produtos/src/main/java/br/com/caelum/vraptor/controller/ProdutoController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import javax.persistence.EntityManager;
 
 import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.dao.ProdutoDao;
 import br.com.caelum.vraptor.model.Produto;
 import br.com.caelum.vraptor.util.JPAUtil;
@@ -13,22 +15,19 @@ import br.com.caelum.vraptor.util.JPAUtil;
 @Controller
 public class ProdutoController {
 	
-	@Path("/produto/sobre")
 	public void sobre() {
 		
 	}
 	
-	@Path("/produto/lista")
 	public List<Produto> lista(){
 		return new ProdutoDao(new JPAUtil().criaEntityManager()).lista();
 	}
 	
-	@Path("/produto/formulario")
 	public void formulario() {
 		
 	}
 	
-	@Path("/produto/adiciona")
+	@Post
 	public void adiciona(Produto produto) {
 		EntityManager em = new JPAUtil().criaEntityManager();
 		em.getTransaction().begin();
@@ -36,7 +35,6 @@ public class ProdutoController {
 		em.getTransaction().commit();
 	}
 	
-	@Path("/produto/remove")
 	public void remove(Produto produto) {
 		EntityManager em = new JPAUtil().criaEntityManager();
 		em.getTransaction().begin();
